4f6b1b66f6553649d5a89caafde522489d0c2abd
Mugglesen <86156726+Mugglesen@users.noreply.github.com>
2021-06-20 19:51:53 +0200

    update yearly blessings

--- commands/blessing.js
+++ commands/blessing.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: 'Blessing',
 	description: 'Handler for all blessings',
 	execute(message, args) {
-        const jones = "0N3OeVPo";
-        const omalley = "kF3S8zKE";
+        const jones = "nDTgymir";
+        const omalley = "a6txXPsM";
         
 		return message.channel.send(`Jones: ${jones}\nO'malley: ${omalley}`);
     },
